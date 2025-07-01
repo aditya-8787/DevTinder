c55c57afe8272ced9e978fa22d98af91684df903
Aditya Singh <aditya7776jnp@gmail.com>
2025-07-01 23:08:27 +0530

    i have made it for sake of my revision

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -1,53 +1,40 @@
 const mongoose = require("mongoose");
 
+// Define schema for connection requests between users
 const connectionRequestSchema = new mongoose.Schema({
+  // ID of the user sending the request
   fromUserId: {
     type: mongoose.Schema.Types.ObjectId,
     required: true,
     ref: "User",
   },
+  // ID of the user receiving the request
   toUserId: {
     type: mongoose.Schema.Types.ObjectId,
     required: true,
     ref: "User",
   },
+  // Current status of the request
   status: {
     type: String,
     required: true,
     enum: {
-      values : ["ignored", "interested", "accepted", "rejected"],
-      message : `{VALUE} is incorrect status type`
+      values: ["ignored", "interested", "accepted", "rejected"], // Only these values allowed
+      message: `{VALUE} is incorrect status type`,
     },
   },
-}, 
-
-{
-  timestamps: true,
+}, {
+  timestamps: true, // Automatically adds createdAt and updatedAt fields
 });
 
+// Prevent duplicate requests between same two users using a compound index
+connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 });
 
-// connectionRequestSchema.pre("save", function (next) {
-//   const connectionRequest = this;
-
-//   // Check if the fromUserId is the same as toUserId
-//   if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
-//     throw new Error("Cannot send connection request to yourself!");
-//   }
-
-//   next();
- 
-  
-// });
-
-
-
-connectionRequestSchema.index({fromUserId : 1 , toUserId :1});
-
-
+// Pre-save hook: prevent users from sending a request to themselves
 connectionRequestSchema.pre("save", function (next) {
   const connectionRequest = this;
 
-  // Check if the fromUserId is the same as toUserId
+  // Throw error if sender and receiver are the same user
   if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
     throw new Error("Cannot send connection request to yourself!");
   }
@@ -55,16 +42,10 @@ connectionRequestSchema.pre("save", function (next) {
   next();
 });
 
-
-
-
-
+// Create and export the model
 const ConnectionRequest = mongoose.model(
   "ConnectionRequest",
   connectionRequestSchema
 );
 
 module.exports = ConnectionRequest;
- 
- 
-    
