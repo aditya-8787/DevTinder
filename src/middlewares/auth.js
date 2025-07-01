c5b7802361c8e63c06de54b6e89bacc6275d51db
Aditya Singh <aditya7776jnp@gmail.com>
2025-07-01 23:09:39 +0530

    ye bhi ho gya

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,52 +1,43 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
- const jwt = require("jsonwebtoken");
+// Middleware to authenticate a user based on JWT stored in cookies
+const userAuth = async (req, res, next) => {
+  try {
+    // Read the token from cookies
+    const { token } = req.cookies;
 
- const User = require("../models/user");
-
-const userAuth = async (req,res,next) => {
- 
-    try {
-
-      // Read the token from the cookies
-  
-    const {token}  = req.cookies;
-
-    if(!token){
-      return res.status(401).send("Please Login !"); 
+    // If no token is found, user is not logged in
+    if (!token) {
+      return res.status(401).send("Please Login !");
     }
 
-    const decodedMessage = await jwt.verify(token , "DEV@Tinder$790");
+    // Verify the token using the secret key
+    const decodedMessage = await jwt.verify(token, "DEV@Tinder$790");
 
-
+    // Extract the user ID from the decoded token payload
     const { _id } = decodedMessage;
 
-    const user = await User.findById( _id );
+    // Find the user in the database by ID
+    const user = await User.findById(_id);
 
-    if(!user){
+    // If user doesn't exist in DB, throw error
+    if (!user) {
       throw new Error("User not found");
     }
 
-    req.user = user;  
+    // Attach the user to the request object so downstream routes can access it
+    req.user = user;
 
+    // Proceed to the next middleware or route handler
     next();
 
-      
-    } catch (error) {
-
-      res.status(400).send("ERROR: " + error.message);
-      
-    }
-
-
-   
-}
-
-
-module.exports = {
-
-  userAuth,
-
+  } catch (error) {
+    // If token is invalid or any error occurs, respond with 400
+    res.status(400).send("ERROR: " + error.message);
+  }
 };
 
-
-  
+module.exports = {
+  userAuth,
+};
